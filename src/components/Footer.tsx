6e590b4cca0bdf0a0d2ff1f05cf0e50a0ad63215
Aman Sharma <167951905+Aman638668@users.noreply.github.com>
2025-08-12 22:37:49 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -127,19 +127,14 @@ export default function Footer() {
               <Mail className="h-5 w-5 text-blue-400 mr-3" />
               <div>
                 <p className="text-sm text-gray-400">Email</p>
-                <a href="mailto:amanuxstudio@gmail.com" className="text-white hover:underline">
-                  amanuxstudio@gmail.com
-                </a>
+                <p className="text-white">amanuxstudio@gmail.com</p>
               </div>
             </div>
-            
             <div className="flex items-center">
               <Phone className="h-5 w-5 text-green-400 mr-3" />
               <div>
                 <p className="text-sm text-gray-400">Phone</p>
-                <a href="tel:+919211104514" className="text-white hover:underline">
-                  +91 9211104514
-                </a>
+                <p className="text-white">+91 9211104514</p>
               </div>
             </div>
 
